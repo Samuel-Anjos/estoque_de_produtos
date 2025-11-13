2b5b7904077129ab29d6d8f6aabe745317eb79df
Samuel Silva <samuelsilvadosanjos22@gmail.com>
2025-11-13 14:12:41 -0300

    simplificando estrutura e conectando o banco de dados MYSQL

--- routes.py
+++ routes.py
@@ -1,38 +1,42 @@
-from flask import render_template, redirect, url_for, Blueprint
+from flask import render_template
+from main import app, get_connection
 
-bp = Blueprint('main', __name__)
-
-@bp.route("/")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    conn = get_connection()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM produto")
+        produtos = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', produtos=produtos)
 
-@bp.route("/produtos")
+@app.route("/produtos")
 def produtos():
     return render_template("produto.html")
 
-@bp.route("/ingredientes")
+@app.route("/ingredientes")
 def ingredientes():
     return render_template("ingredientes.html")
 
-@bp.route("/baixa")
+@app.route("/baixa")
 def baixa():
     return render_template("baixa.html")
 
-@bp.route("/venda")
+@app.route("/venda")
 def venda():
     return render_template("venda.html")
 
-@bp.route("/add_ingrediente")
+@app.route("/add_ingrediente")
 def add_ingrediente():
     return render_template("add-ingrediente.html")
 
-@bp.route("/editar-produto/<int:id>")
+@app.route("/editar-produto/<int:id>")
 def editar_produto(id):
     # edição do banco de dados (colocar lógica em breve)
     # return render_template("editar-produto.html", id=id)
     pass
 
-@bp.route("/excluir-produto/<int:id>")
+@app.route("/excluir-produto/<int:id>")
 def excluir_produto(id):
     # exclusão do banco de dados(colocar lógica em breve)
     # return redirect(url_for("produtos"))
